323275ee78a8f78faf77736374a84c26d0f512ea
Rasmus Munk <munk1@live.dk>
2019-08-07 17:24:40 +0200

    Add tmpfs jupyterhub config examples

---
+++ examples/jupyter_config_tmpfs_mounts.py
@@ -0,0 +1,30 @@
+# Configuration file for jupyterhub.
+import os
+
+c = get_config()
+
+c.JupyterHub.spawner_class = 'jhub.SwarmSpawner'
+c.JupyterHub.authenticator_class = 'jhubauthenticators.DummyAuthenticator'
+c.DummyAuthenticator.password = 'password'
+
+c.JupyterHub.ip = '0.0.0.0'
+c.JupyterHub.hub_ip = '0.0.0.0'
+
+c.JupyterHub.cleanup_servers = False
+
+# First pulls can be really slow, so let's give it a big timeout
+c.SwarmSpawner.start_timeout = 60 * 5
+
+c.SwarmSpawner.jupyterhub_service_name = 'jupyterhub'
+c.SwarmSpawner.networks = ["jupyterhub_default"]
+
+tmpfs_mounts  = [{'type': 'tmpfs',
+                  'source': '',
+                  'target': '/tmp/test_mount',
+                  'tmpfs_size': '1048576'}]
+
+c.SwarmSpawner.container_spec = {
+    # The command to run inside the service
+    'env': {'JUPYTER_ENABLE_LAB': '1'},
+    'mounts': tmpfs_mounts
+}
