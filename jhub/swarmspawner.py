79265d34ddf2a49bd6df3b5c0d37d61e38f678ce
Rasmus Munk <munk1@live.dk>
2021-06-04 20:15:58 +0200

    In jhub 1.4.0 the _env_keep_default traitlet requires a parameter

--- jhub/swarmspawner.py
+++ jhub/swarmspawner.py
@@ -286,7 +286,7 @@ class SwarmSpawner(Spawner):
         self.service_id = ""
 
     @staticmethod
-    def _env_keep_default():
+    def _env_keep_default(param):
         """it's called in traitlets. It's a special method name.
         Don't inherit any env from the parent process"""
         return []
