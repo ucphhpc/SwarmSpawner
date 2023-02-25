86411ac5631b5b12a858125b87743d9a63d979da
Rasmus Munk <munk1@live.dk>
2023-02-25 15:17:49 +0100

    Add the ulimit container spec tests

---
+++ tests/configs/ulimit_jupyterhub_config.py
@@ -0,0 +1,35 @@
+c = get_config()
+
+c.JupyterHub.ip = "0.0.0.0"
+c.JupyterHub.hub_ip = "0.0.0.0"
+
+# Authenticator
+c.JupyterHub.authenticator_class = "jhubauthenticators.DummyAuthenticator"
+c.DummyAuthenticator.password = "just magnets"
+
+c.JupyterHub.spawner_class = "jhub.SwarmSpawner"
+
+# First pulls can be really slow, so let's give it a big timeout
+c.SwarmSpawner.start_timeout = 60 * 15
+
+c.SwarmSpawner.jupyterhub_service_name = "jupyterhub"
+
+c.SwarmSpawner.networks = ["jh_test"]
+
+# Before the user can select which image to spawn,
+# user_options has to be enabled
+c.SwarmSpawner.use_user_options = True
+
+# Available docker images the user can spawn
+c.SwarmSpawner.images = [
+    {"image": "ucphhpc/base-notebook:latest", "name": "Basic Python Notebook"},
+    {"image": "ucphhpc/base-notebook:latest", "name": "Basic Python Notebook 2"},
+]
+
+# -1 should mean unlimited
+c.SwarmSpawner.container_spec = {
+    "ulimit": {
+        "soft": "-1",
+        "hard": "-1"
+    }
+}
