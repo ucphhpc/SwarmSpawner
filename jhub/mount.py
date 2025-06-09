a6edf75fd2e995b94ffcc5af130161b00dd50928
Rasmus Munk <rasmunk@users.noreply.github.com>
2025-06-09 10:41:23 +0200

    Update the mount objects to no longer use the traitlets Config class to construct mount configs since the underlying copy mechanism has changed which doesn't give the right level of assurance that deepcopy will work as expected when the Mount classes use template configs that should remain unaltered

--- jhub/mount.py
+++ jhub/mount.py
@@ -1,61 +1,77 @@
 import copy
 from tornado import gen
-from traitlets.config import LoggingConfigurable, Config
+from traitlets.config import LoggingConfigurable
 from docker.types import DriverConfig, Mount
 from jhub.util import recursive_format
 
 
 class Mounter(LoggingConfigurable):
-    def __init__(self, config):
+    def __init__(self, mount_config):
         LoggingConfigurable.__init__(self)
-        if not isinstance(config, dict):
+        if not isinstance(mount_config, dict):
             raise Exception("A dictionary typed config is expected")
-        if not config:
+        if not mount_config:
             raise Exception("A non-zero sized dictionary is expected")
         # Ensure that we don't change the passed in config,
         # But only use it. Deep copy is allowed if it is of type Config
-        self.log.debug("instantiating Mounter with config: {}".format(config))
-        self.config = copy.deepcopy(Config(config))
+        self.log.debug("instantiating Mounter with config: {}".format(mount_config))
+        self._mount_config = mount_config
+
+    @property
+    def mount_config(self):
+        self.log.debug("Retriving mount config: {}".format(self._mount_config))
+        return self._mount_config
+
+    @mount_config.setter
+    def mount_config(self, mount_config):
+        self.log.debug(
+            "Setting config: {} overwriting: {}".format(
+                self._mount_config, mount_config
+            )
+        )
+        self._mount_config = mount_config
 
     @gen.coroutine
     def gen_config_copy(self):
-        return copy.deepcopy(self.config)
+        return copy.deepcopy(self.mount_config)
 
     @gen.coroutine
-    def format_config(self, config, **kwargs):
+    def format_config(self, mount_config, **kwargs):
         # Dynamically overload the mount config
-        self.log.debug("formatting mount config: {} with: {}".format(config, kwargs))
+        self.log.debug(
+            "formatting mount config: {} with: {}".format(mount_config, kwargs)
+        )
         for key, value in kwargs.items():
-            recursive_format(config, value)
-        self.log.debug("new formatted config: {}".format(config))
+            recursive_format(mount_config, value)
+        self.log.debug("new formatted config: {}".format(mount_config))
 
 
 class VolumeMounter(Mounter):
-    def __init__(self, config):
-        Mounter.__init__(self, config)
+    def __init__(self, mount_config):
+        Mounter.__init__(self, mount_config)
 
     @gen.coroutine
-    def create_mount(self, config):
-        mount = {}
-        mount.update(config)
-        return Mount(**mount)
+    def create_mount(self, mount_config):
+        mount_settings = {}
+        mount_settings.update(mount_config)
+        return Mount(**mount_settings)
 
     @gen.coroutine
-    def create(self, **format_config_kwargs):
+    def create(self, **format_mount_config_kwargs):
         self.log.debug(
-            "Creating VolumeMount with options {}".format(format_config_kwargs)
+            "Creating VolumeMount with options {}".format(format_mount_config_kwargs)
         )
-        new_config = yield self.gen_config_copy()
-        yield self.format_config(new_config, **format_config_kwargs)
-        yield self.validate_config(new_config)
-        mount = yield self.create_mount(new_config)
+        new_mount_config = yield self.gen_config_copy()
+        yield self.format_config(new_mount_config, **format_mount_config_kwargs)
+        yield self.validate_config(new_mount_config)
+        mount = yield self.create_mount(new_mount_config)
         return mount
 
     @gen.coroutine
-    def validate_config(self, config):
+    def validate_config(self, mount_config):
         self.log.debug("validate_config")
         required_config_keys = ["source", "target"]
-        missing_keys = [key for key in required_config_keys if key not in config]
+        missing_keys = [key for key in required_config_keys if key not in mount_config]
 
         if missing_keys:
             self.log.error("Missing configure keys {}".format(",".join(missing_keys)))
@@ -64,7 +80,7 @@ class VolumeMounter(Mounter):
             )
 
         required_config_values = ["target"]
-        empty_values = [key for key in required_config_values if not config[key]]
+        empty_values = [key for key in required_config_values if not mount_config[key]]
         if empty_values:
             self.log.error(
                 "Missing configuring values {}".format(",".join(empty_values))
@@ -75,30 +91,30 @@ class VolumeMounter(Mounter):
 
 
 class SSHFSMounter(Mounter):
-    def __init__(self, config):
-        Mounter.__init__(self, config)
+    def __init__(self, mount_options):
+        Mounter.__init__(self, mount_options)
 
     @gen.coroutine
-    def create_mount(self, config):
-        self.log.debug("create_mount from config: {}".format(config))
+    def create_mount(self, mount_config):
+        self.log.debug("create_mount from config: {}".format(mount_config))
         # Adapt mount options into appropriate types
         driver_config = DriverConfig(
-            config["driver_config"]["name"],
-            config["driver_config"]["options"],
+            mount_config["driver_config"]["name"],
+            mount_config["driver_config"]["options"],
         )
 
-        mount_config = {}
-        mount_config.update(config)
+        mount_settings = {}
+        mount_settings.update(mount_config)
         # Override the DriverConfig to be the correct type
         # as expected by the Docker module.
-        mount_config["driver_config"] = driver_config
-        return Mount(**mount_config)
+        mount_settings["driver_config"] = driver_config
+        return Mount(**mount_settings)
 
     @gen.coroutine
-    def validate_config(self, config):
+    def validate_config(self, mount_config):
         self.log.debug("validate_config")
         required_config_keys = ["source", "target", "type", "driver_config"]
-        missing_keys = [key for key in required_config_keys if key not in config]
+        missing_keys = [key for key in required_config_keys if key not in mount_config]
 
         if missing_keys:
             self.log.error("Missing configure keys {}".format(",".join(missing_keys)))
@@ -107,7 +123,7 @@ class SSHFSMounter(Mounter):
             )
 
         required_config_values = ["type", "driver_config", "target"]
-        empty_values = [key for key in required_config_values if not config[key]]
+        empty_values = [key for key in required_config_values if not mount_config[key]]
         if empty_values:
             self.log.error(
                 "Missing configuring values {}".format(",".join(empty_values))
@@ -117,9 +133,9 @@ class SSHFSMounter(Mounter):
             )
 
     @gen.coroutine
-    def create(self, **format_config_kwargs):
+    def create(self, **format_mount_config_kwargs):
         new_config = yield self.gen_config_copy()
-        yield self.format_config(new_config, **format_config_kwargs)
+        yield self.format_config(new_config, **format_mount_config_kwargs)
         yield self.validate_config(new_config)
         mount = yield self.create_mount(new_config)
         return mount
