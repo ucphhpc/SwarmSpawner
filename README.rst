89b20b32e0ae0764a8f522412b700a56d85057b0
Rasmus Munk <rasmunk@users.noreply.github.com>
2025-09-05 11:43:44 +0200

    Remove the py37, and py38 from the classifiers

--- README.rst
+++ README.rst
@@ -15,7 +15,7 @@ More info about Docker Services `here <https://docs.docker.com/engine/reference/
 Prerequisites
 ================
 
-Python version 3.3 or above is required.
+Python version 3.9 or above is required.
 
 
 Installation
--- setup.py
+++ setup.py
@@ -43,11 +43,10 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
 )
